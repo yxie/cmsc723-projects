2201eb99677aa1e8051df8597a3b3f097cd19c8d
Yang <yxie.ece@gmail.com>
2017-10-24 12:26:55 -0400

    Implement transition-based dependency parser
    1. Implement arc-standard parser
    2. Implement the basic framework for peceptron
    
    Next step:
    1. handle non-projective trees
    2. finish the peceptron train/test code

---
+++ PJ2/transparser.py
@@ -0,0 +1,256 @@
+def readDataset(subset):
+    data = []
+    sentence = []
+    dependents_of_word = dict()
+    if subset in ['tr', 'tr100', 'dev', 'tst']:
+        with open('en.' + subset) as lines:
+            for line in lines:
+                if line == '\n':
+                    data.append((sentence, dependents_of_word))
+                    sentence = []
+                    dependents_of_word = dict()
+                else:
+                    word_fields = line.split('\t')
+                    sentence.append(word_fields)
+                    head = word_fields[6]
+                    dependent = word_fields[0]
+                    if head in dependents_of_word:
+                        dependents_of_word[head].append(dependent)
+                    else:
+                        dependents_of_word[head] = [dependent]
+        return data
+    else:
+        print '>>>> invalid input !!! <<<<<'
+
+def leftArc(stack, dependents_of_word):
+    assert len(stack) >= 2 and stack[-2] != 0
+    dependents_of_word[stack[-1]].remove(stack[-2])
+    del stack[-2]
+    return (stack, dependents_of_word)
+
+def rightArc(stack, dependents_of_word):
+    assert len(stack) >= 2 and stack[-1] != 0
+    dependents_of_word[stack[-2]].remove(stack[-1])
+    del stack[-1]
+    return (stack, dependents_of_word)
+
+def shift(stack, buff):
+    assert len(buff) > 0
+    stack.append(buff[0])
+    del buff[0]
+    return (stack ,buff)
+
+
+def isHeadDependent(idx1, idx2, dependents_of_word):
+    return (idx2 in dependents_of_word.get(idx1, []))
+
+def hasNoDependent(idx, dependents_of_word):
+    return dependents_of_word.get(idx, []) == []
+
+def getFeatures(stack, buff, sentence):
+    # get features
+    # corner cases, stack only has root
+    if stack[-1] == '0':
+        stack_top_word = 'ROOT'
+        stack_top_pos = 'ROOT_POS'
+    else:
+        stack_word_idx = int(stack[-1]) - 1 # -1 because word_index starts from 1
+        stack_top_word = sentence[stack_word_idx][1]
+        stack_top_pos = sentence[stack_word_idx][4]
+    # corner cases, buffer is empty
+    if buff == []:
+        buff_head_word = 'EMPTY'
+        buff_head_pos = 'EMPTY_POS'
+    else:
+        buff_word_idx = int(buff[0]) - 1 # -1 because word_index starts from 1
+        buff_head_word = sentence[buff_word_idx][1]
+        buff_head_pos = sentence[buff_word_idx][4]
+    word_pair = (stack_top_word, buff_head_word)
+    pos_pair = (stack_top_pos, buff_head_pos)
+    features = [stack_top_word, buff_head_word, stack_top_pos, buff_head_pos,
+                word_pair, pos_pair, 'bias']
+    # print features
+    # raw_input()
+    return features
+
+def getPredictedTransition(features, weights):
+    # compute scores
+    scores = [0] * len(weights)
+    for i, weight in enumerate(weights):
+        for f in features:
+            scores[i] += weight.get(f, 0)
+    # return index with max score
+    return scores.index(max(scores))
+
+def getTrueTransition(stack, buff, dependents_of_word, quiet=1):
+    true_trans = -1
+    if len(stack) >= 2:
+        s1_idx = stack[-1]
+        s2_idx = stack[-2]
+        if quiet == 0:
+            print 'info:'
+            print s1_idx, s2_idx
+            for head in dependents_of_word:
+                print head, ": ", dependents_of_word[head]
+        if (isHeadDependent(s1_idx, s2_idx, dependents_of_word) # head-dependent relation
+            and s2_idx != 0 # and is not root
+            and hasNoDependent(s2_idx, dependents_of_word)): # s2 has no dependents
+            (stack, dependents_of_word) = leftArc(stack, dependents_of_word)
+            true_trans = 0
+        elif (isHeadDependent(s2_idx, s1_idx, dependents_of_word)
+            and s1_idx != 0
+            and hasNoDependent(s1_idx, dependents_of_word)):
+            (stack, dependents_of_word) = rightArc(stack, dependents_of_word)
+            true_trans = 1
+        elif len(buff) > 0:
+            (stack, buff) = shift(stack, buff)
+            true_trans = 2
+        else:
+            true_trans = -1 # non-projective
+    elif len(buff) > 0:
+        (stack, buff) = shift(stack, buff)
+        true_trans = 2
+    else:
+        true_trans = -1 # non-projective
+    # assert true_trans != -1
+    return true_trans
+
+def updateWeights(m, weights, pred_trans, true_trans, features):
+    for f in features:
+        weights[pred_trans][f] = weights[pred_trans].get(f, 0) - 1
+        weights[true_trans][f] = weights[true_trans].get(f, 0) + 1
+        m[pred_trans][f] += weights[pred_trans][f]
+        m[true_trans][f] += weights[true_trans][f]
+    return (m, weights)
+
+def train(train_data, weights):
+    m = weights
+    t = 0
+    # process one iteration
+    for (sentence, dependents_of_word)  in train_data:
+        if quiet == 0:
+            print 'word info:'
+            for word in sentence:
+                print word
+            print 'head-dependent info:'
+            for head in dependents_of_word:
+                print head, ": ", dependents_of_word[head]
+            raw_input('\n')
+
+        # process one sentence
+        buff = [word[0] for word in sentence] # all word indices
+        stack = ['0'] # root index
+        transition = [] # empty
+        while buff != [] or stack != ['0']: # if buff is not empty and stack has other values than root
+            ## before
+            if quiet == 0:
+                print 'Before: '
+                print 'Buffer:', buff
+                print 'Stack:', stack
+                print 'Transition:', transition
+            ## create training data and learn
+            ## get features
+            features = getFeatures(stack, buff, sentence)
+            ## get predicted output
+            pred_trans = getPredictedTransition(features, weights)
+            ## get correct output
+            true_trans = getTrueTransition(stack, buff, dependents_of_word)
+            transition.append(true_trans)
+            ## after
+            if quiet == 0:
+                print 'After: '
+                print 'Buffer:', buff
+                print 'Stack:', stack
+                print 'Transition:', transition
+                raw_input('>>>stop here<<<\n')
+            ## update weights
+            if pred_trans != true_trans:
+                (m, weights) = updateWeights(m, weights, pred_trans, true_trans, features)
+                t += 1
+    # recover weights after one iteration
+    for i in range(len(weights)):
+        for feat in weight:
+            weights[i][feat] = m[i][feat] / t
+    return weights
+
+
+def testOracleParser(train_data, quiet):
+    i = 0
+    test_idx = 31
+    for (sentence, dependents_of_word)  in train_data:
+        #if i == test_idx:
+        #    quiet = 0
+        i += 1
+        if quiet == 0:
+            print 'word info:'
+            for word in sentence:
+                print word
+            print 'head-dependent info:'
+            for head in dependents_of_word:
+                print head, ": ", dependents_of_word[head]
+            raw_input('\n')
+
+        # process one sentence
+        buff = [word[0] for word in sentence] # all word indices
+        stack = ['0'] # root index
+        transition = [] # empty
+        true_trans = 0
+        while (buff != [] or stack != ['0']) and true_trans != -1: # if buff is not empty and stack has other values than root
+            ## before
+            if quiet == 0:
+                print 'Before: '
+                print 'Buffer:', buff
+                print 'Stack:', stack
+                print 'Transition:', transition
+            ## get correct output
+            true_trans = getTrueTransition(stack, buff, dependents_of_word, quiet)
+            transition.append(true_trans)
+            ## after
+            if quiet == 0:
+                print 'After: '
+                print 'Buffer:', buff
+                print 'Stack:', stack
+                print 'Transition:', transition
+                raw_input('>>>stop here<<<\n')
+            if true_trans == -1:
+                print 'non-projective examples', train_data.index((sentence, dependents_of_word))
+
+
+
+if __name__ == "__main__":
+    # print something for debugging
+    quiet = 1
+
+    # read dataset => train_data, dev_data, test_data
+    # data[sentence_id][word_id][field_id]
+    train_data = readDataset('tr100')
+
+    # for each sentence
+    #   update configuration
+    #   obtain its transition (i.e. labels) guided by oracle
+    #   based on configuration - transition pairs
+    #   train the model using perceptron
+
+    # initialize weights for features
+    #   identity of word at top of the stack
+    #   identity of word at head of buffer
+    #   coarse POS (field 4) of word at top of the stack
+    #   coarse POS (field 4) of word at head of buffer
+    #   pair of words at top of stack and head of buffer
+    #   pair of coarse POS (field 4) at top of stack and head of buffer
+
+    # define leftArc = 0, rightArc = 1, shift = 2
+    '''
+    weight_leftArc = dict()
+    weight_rightArc = dict()
+    weight_shift = dict()
+    weights = [weight_leftArc, weight_rightArc, weight_shift]
+    for iter in range(1):
+        print 'iteration =', iter
+        # train the model
+        weights = train(train_data, weights)
+        # test the model
+        test(dev_data, weights)
+    '''
+
+    testOracleParser(train_data, quiet)
